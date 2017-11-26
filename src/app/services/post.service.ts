aae97a688d79503e81ef42cd14c730a943b1c7a3
Alison Vilela <alison.vilela@live.nl>
2017-11-25 23:08:33 -0200

    Test CircleCI

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Rx'
 import { HttpClient, HttpParams } from '@angular/common/http'
 
